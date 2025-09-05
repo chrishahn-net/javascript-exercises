931602ded18a4e03c61890a67a4c1febb26e4ce9
Chris Hahn <git@chrishahn.net>
2025-09-05 16:02:06 -0400

    Complete "06_repeatString"

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = (word, times) => {
+  if (times < 0) return "ERROR";
+  let repeatedString = "";
+  for (let i = 0; i < times; i++) {
+    repeatedString += word;
+  }
+  return repeatedString
 };
 
 // Do not edit below this line
