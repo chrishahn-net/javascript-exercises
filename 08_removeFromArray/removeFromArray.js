1f9a6726f6c2275fda118dc6c500c9b2f0edc738
Chris Hahn <git@chrishahn.net>
2025-09-05 16:03:06 -0400

    Complete "08_removeFromArray"

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(arr, ...itemsToRemove) {
+  return arr.filter((item) => !itemsToRemove.includes(item));
+}
+  // find item to remove from array
+  // remove item from array
 
+console.log(removeFromArray([1, 2, 3, 4], 3, 2));
 // Do not edit below this line
 module.exports = removeFromArray;
