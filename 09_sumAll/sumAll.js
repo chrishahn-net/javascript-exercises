955cc8878205f74237f0fbcab51f684d77c0a8b1
Chris Hahn <git@chrishahn.net>
2025-09-06 12:13:36 -0400

    Complete "09_sumAll"

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,16 @@
-const sumAll = function() {
+const sumAll = function(...nums) {
+  let total = 0;
+  nums.sort((a, b) => a - b);
+  if (nums.some((num) => num < 0 || !Number.isInteger(num))) return "ERROR";
 
+  for (let i = nums[0]; i <= nums[1]; i++) {
+    total += i;
+  }
+
+  return total;
 };
+
+console.log( sumAll(4, -2.5) )
 
 // Do not edit below this line
 module.exports = sumAll;
