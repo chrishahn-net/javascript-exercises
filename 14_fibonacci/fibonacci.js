4498e66cdb1f2f5acac5755bce21f0224ed617e6
Chris Hahn <git@chrishahn.net>
2025-09-15 16:40:04 -0400

    Complete "14_fibonacci" exercise

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(fibMember) {
+  fibMember = +fibMember;
+  if (fibMember < 0) return "OOPS";
+  if (fibMember === 0) return 0;
+  let prev = 0;
+  let curr = 1;
+  let next = 1;
+  for (let i = 1; i < fibMember; i++) {
+    next = prev + curr;
+    prev = curr;
+    curr = next;
+  }
+  return next;
 };
 
 // Do not edit below this line
