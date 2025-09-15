dedc6006ebccb9dbb545f3bb2ee300c5c3563a61
Chris Hahn <git@chrishahn.net>
2025-09-15 16:10:28 -0400

    Complete "13_palindromes" exercise

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function(string) {
+  const punctuation = /[^a-z0-9]/g;
+  const cleanedString = string.toLowerCase().replace(punctuation, "");
+  const reversedString = cleanedString.split("").reverse().join("");
+  return cleanedString === reversedString;
 };
+
+palindromes("A car, a man, a maraca.");
 
 // Do not edit below this line
 module.exports = palindromes;
