6c3d042e6ef2b416b077755378ebb39d6bb353b7
Chris Hahn <git@chrishahn.net>
2025-09-18 09:31:36 -0400

    Complete "16_findTheOldest" exercise

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
+const getAge = function (birth, death) {
+  if (!death) {
+    death = new Date().getFullYear();
+  }
+  return death - birth;
+}
 
+const findTheOldest = function (array) {
+  return array.reduce((oldest, current) => {
+    let oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath)
+    let currentAge = getAge(current.yearOfBirth, current.yearOfDeath);
+    return oldestAge > currentAge ? oldest : current;
+  })
 };
 
 // Do not edit below this line
